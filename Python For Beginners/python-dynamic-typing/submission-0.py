edc071971bdcd1877cf50a66566925933fd96056
deep-purple-zip <149053818+deep-purple-zip@users.noreply.github.com>
2026-05-01 20:46:19 +0300

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,15 @@
+variable: int | float | bool | str | list
+variable = int()
+print(type(variable))
+
+variable = float()
+print(type(variable))
+
+variable = bool()
+print(type(variable))
+
+variable = str()
+print(type(variable))
+
+variable = list()
+print(type(variable))
