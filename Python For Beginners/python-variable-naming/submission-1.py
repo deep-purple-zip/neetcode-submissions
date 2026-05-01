975c4320f741f4c335b2fda7e4bf9fd15e86d0e1
deep-purple-zip <149053818+deep-purple-zip@users.noreply.github.com>
2026-05-01 20:33:55 +0300

    Add: python-variable-naming - submission-1

---
+++ Python For Beginners/python-variable-naming/submission-1.py
@@ -0,0 +1,7 @@
+capital_of_spain: str = "Madrid"
+capital_of_france: str = "Paris"
+capital_of_germany: str = "Berlin"
+
+print(capital_of_spain)
+print(capital_of_france)
+print(capital_of_germany)
