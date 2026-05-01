6ad775eb8ba4d456e883e6f1935bf44752b61864
deep-purple-zip <149053818+deep-purple-zip@users.noreply.github.com>
2026-05-01 20:35:25 +0300

    Add: python-naming-conventions - submission-0

---
+++ Python For Beginners/python-naming-conventions/submission-0.py
@@ -0,0 +1,7 @@
+python_creation_date: str = "February, 1991"
+java_creation_date: str = "May, 1995"
+javascript_creation_date: str = "December, 1995"
+
+print(python_creation_date)
+print(java_creation_date)
+print(javascript_creation_date)
