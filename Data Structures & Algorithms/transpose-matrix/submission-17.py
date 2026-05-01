ac36801d47d877e3af590e149b3e9cd75df9a0ef
deep-purple-zip <149053818+deep-purple-zip@users.noreply.github.com>
2026-05-01 23:58:25 +0300

    Add: transpose-matrix - submission-17

---
+++ Data Structures & Algorithms/transpose-matrix/submission-17.py
@@ -0,0 +1,13 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        n: int = len(matrix)
+        m: int = len(matrix[0])
+        # if 1 <= m <= 1_000 and if 1 <= m <= 1_000 and 1 <= m * n <= 100_000:
+        result: list[list[int]] = []
+    
+        for j in range(m):
+            row: list[int] = []
+            for i in range(n):
+                row.append(matrix[i][j])
+            result.append(row)
+        return result
