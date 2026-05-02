2ce388faf4555bbea99b210ccfbd5fef48e22cdc
deep-purple-zip <149053818+deep-purple-zip@users.noreply.github.com>
2026-05-02 14:37:56 +0300

    Add: python-string-looping-shorthand - submission-0

---
+++ Python For Beginners/python-string-looping-shorthand/submission-0.py
@@ -0,0 +1,8 @@
+def print_string_characters(word1: str, word2: str) -> None:
+    print(*word1, *word2, sep="\n")
+
+
+
+
+# do not modify below this line
+print_string_characters("Hello, World!", "Good Job!")
