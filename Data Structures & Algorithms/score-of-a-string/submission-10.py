d7d60782a2c06383b5dc2d2727143b07c1c21c3c
deep-purple-zip <149053818+deep-purple-zip@users.noreply.github.com>
2026-05-02 13:05:39 +0300

    Add: score-of-a-string - submission-10

---
+++ Data Structures & Algorithms/score-of-a-string/submission-10.py
@@ -0,0 +1,9 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        length: int = len(s)
+        if 2 <= length <= 100 and s.islower():
+            sum: int = 0
+            for i in range(length - 1):
+                sum += abs(ord(s[i]) - ord(s[i+1]))
+            return sum
+        return 0
