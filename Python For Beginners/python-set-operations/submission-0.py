e1c49dc06d87270f7eaa069d771487b4699ca2a4
deep-purple-zip <149053818+deep-purple-zip@users.noreply.github.com>
2026-05-03 18:04:44 +0300

    Add: python-set-operations - submission-0

---
+++ Python For Beginners/python-set-operations/submission-0.py
@@ -0,0 +1,8 @@
+def count_unique_words(words: list[str]) -> int:
+    return len(set(words))
+
+# do not modify code below this line
+print(count_unique_words(["hello", "world", "hello", "goodbye"]))
+print(count_unique_words(["hello", "world", "i", "am", "world"]))
+print(count_unique_words(["hello", "hello", "hello"]))
+print(count_unique_words([]))
