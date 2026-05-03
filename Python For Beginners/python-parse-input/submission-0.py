0f6873d4f3e2626cae825b3cc90a62bc0ad077e6
deep-purple-zip <149053818+deep-purple-zip@users.noreply.github.com>
2026-05-03 19:57:59 +0300

    Add: python-parse-input - submission-0

---
+++ Python For Beginners/python-parse-input/submission-0.py
@@ -0,0 +1,7 @@
+def read_integers() -> list[int]:
+    return [int(number) for number in input().split(",")]
+
+# do not modify the code below
+print(read_integers())
+print(read_integers())
+print(read_integers())
