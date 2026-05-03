47357ecb57161495fafc42f9e8ed340317a02008
deep-purple-zip <149053818+deep-purple-zip@users.noreply.github.com>
2026-05-03 18:12:54 +0300

    Add: python-intro-to-dictionaries - submission-0

---
+++ Python For Beginners/python-intro-to-dictionaries/submission-0.py
@@ -0,0 +1,16 @@
+def create_dict(name: str, age: int) -> dict[str, int]:
+    return {name: age}
+
+
+def list_to_dict(words: list[str]) -> dict[str, int]:
+    return {word: index for index, word in enumerate(words)}
+
+
+
+# don't modify code below this line
+print(create_dict("Alice", 25))
+print(create_dict("Jane", 35))
+print(create_dict("Joe", 45))
+
+print(list_to_dict(["Alice", "Jane", "Joe"]))
+print(list_to_dict(["Apple", "Banana", "Watermelon", "Pineapple"]))
